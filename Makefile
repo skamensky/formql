0a0c95589d94dd2246e1f3c1a77d7a17801b6948
skamensky <shmuelkamensky@gmail.com>
2026-04-22 22:34:31 +0300

    Add temporary agent handoff for local Docker follow-up

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ SCHEMA_PATH ?= examples/catalogs/rental-agency.formql.schema.json
 VSCODE_EXTENSION_DIR ?= editors/vscode
 VSCODE_EXTENSION_VSIX ?= $(VSCODE_EXTENSION_DIR)/formql-vscode-0.1.0.vsix
 
-.PHONY: go-test db-up db-down db-reset catalog ast hir typecheck query lsp lsp-offline typecheck-offline vscode-extension-package vscode-extension-install vscode-extension-reinstall
+.PHONY: go-test db-up db-down db-reset ext-db-up ext-db-down ext-docker-build ext-e2e catalog ast hir typecheck query lsp lsp-offline typecheck-offline vscode-extension-package vscode-extension-install vscode-extension-reinstall
 
 go-test:
 	go test ./...
@@ -51,3 +51,21 @@ vscode-extension-install:
 	code --install-extension "$(VSCODE_EXTENSION_VSIX)" --force
 
 vscode-extension-reinstall: vscode-extension-package vscode-extension-install
+
+
+ext-docker-build:
+	docker compose build formula-db-ext
+
+ext-db-up:
+	docker compose up -d formula-db-ext
+
+ext-db-down:
+	docker compose rm -sf formula-db-ext
+
+ext-e2e: ext-docker-build ext-db-up
+	docker compose cp docker/extension/smoke.sql formula-db-ext:/tmp/formql-smoke.sql
+	docker compose exec -T formula-db-ext psql -U formula -d formula -v ON_ERROR_STOP=1 -f /tmp/formql-smoke.sql
+
+
+ext-build:
+	make -C ext/formql
---
+++ ext/formql/src/formql.c
@@ -0,0 +1,85 @@
+#include "postgres.h"
+
+#include "fmgr.h"
+#include "lib/stringinfo.h"
+#include "nodes/parsenodes.h"
+#include "parser/parser.h"
+#include "utils/builtins.h"
+#include "utils/json.h"
+
+PG_MODULE_MAGIC;
+
+PG_FUNCTION_INFO_V1(formql_verify_sql_error);
+PG_FUNCTION_INFO_V1(formql_verify_sql_ok);
+PG_FUNCTION_INFO_V1(formql_verify_sql_diagnostics);
+
+typedef struct VerifyFailure {
+	char *message;
+	char sqlstate[6];
+} VerifyFailure;
+
+static bool verify_sql_syntax(const char *sql, VerifyFailure *failure) {
+	PG_TRY();
+	{
+		raw_parser(sql, RAW_PARSE_DEFAULT);
+		return true;
+	}
+	PG_CATCH();
+	{
+		ErrorData *edata = CopyErrorData();
+		FlushErrorState();
+		failure->message = pstrdup(edata->message ? edata->message : "parse failure");
+		memcpy(failure->sqlstate, unpack_sql_state(edata->sqlerrcode), 5);
+		failure->sqlstate[5] = '\0';
+		FreeErrorData(edata);
+		return false;
+	}
+	PG_END_TRY();
+}
+
+static Datum diagnostics_jsonb_datum(const VerifyFailure *failure) {
+	StringInfoData json;
+	initStringInfo(&json);
+	appendStringInfoString(&json, "{\"ok\":false,\"diagnostics\":[{\"message\":\"");
+	escape_json(&json, failure->message);
+	appendStringInfoString(&json, "\",\"sqlstate\":\"");
+	escape_json(&json, failure->sqlstate);
+	appendStringInfoString(&json, "\"}]}");
+	return DirectFunctionCall1(jsonb_in, CStringGetDatum(json.data));
+}
+
+Datum formql_verify_sql_error(PG_FUNCTION_ARGS) {
+	text *sql_text = PG_GETARG_TEXT_PP(0);
+	char *sql = text_to_cstring(sql_text);
+	VerifyFailure failure;
+
+	if (verify_sql_syntax(sql, &failure)) {
+		PG_RETURN_NULL();
+	}
+
+	PG_RETURN_TEXT_P(cstring_to_text(failure.message));
+}
+
+Datum formql_verify_sql_ok(PG_FUNCTION_ARGS) {
+	text *sql_text = PG_GETARG_TEXT_PP(0);
+	char *sql = text_to_cstring(sql_text);
+	VerifyFailure failure;
+
+	if (verify_sql_syntax(sql, &failure)) {
+		PG_RETURN_BOOL(true);
+	}
+
+	PG_RETURN_BOOL(false);
+}
+
+Datum formql_verify_sql_diagnostics(PG_FUNCTION_ARGS) {
+	text *sql_text = PG_GETARG_TEXT_PP(0);
+	char *sql = text_to_cstring(sql_text);
+	VerifyFailure failure;
+
+	if (verify_sql_syntax(sql, &failure)) {
+		PG_RETURN_NULL();
+	}
+
+	PG_RETURN_DATUM(diagnostics_jsonb_datum(&failure));
+}
